0f8190f34b5d899a0b82aa6b7cdb9d28eac88431
Chris Ball <chris.ball@terenc.com>
2025-09-13 14:22:45 -0500

    The first publish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,5 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [react()],
-    adapter: netlify()
+  output: 'static',
+  // site: 'https://ballforbloomfield.com' // optional, add later if you want OG/sitemap
 });
